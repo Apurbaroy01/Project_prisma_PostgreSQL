38d43396b614b138e750e007ae23e565fc44ddb2
Apurbaroy01 <apurbaroy00011@gmail.com>
2026-09-20 20:21:54 +0600

    feat: implement sendResponse utility and update registerUser response handling

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -2,17 +2,18 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { userService } from "./user.services";
 import { catchAsync } from "../../utils/catchAsync";
 import httpStatus from "http-status"
+import { sendResponse } from "../../utils/sendResponse";
 
 
 const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-
     const user = await userService.registeruserIntoDB(payload)
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
         success: true,
-        message: "user success register",
-        data: user
+        statusCode: httpStatus.CREATED,
+        message: "User created successfully",
+        data: {user}
     })
 })
 
